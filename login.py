770d0cc75eb79ad8e10e213f253b901f9121d7b4
Stas <stasnesterov13@gmail.com>
2024-04-09 16:22:26 +0300

    change loadUi

--- login.py
+++ login.py
@@ -1,29 +1,12 @@
 import sys
-import subprocess
 from PyQt6.QtWidgets import QApplication, QDialog
+from PyQt6 import uic
 
-
-# Конвертация .ui файла в .py
-def convert_ui_to_py(ui_file, py_file):
-    try:
-        subprocess.run(["pyuic6", "-x", ui_file, "-o", py_file], check=True)
-        print(f"Файл {ui_file} успешно конвертирован в {py_file}")
-    except subprocess.CalledProcessError as e:
-        print(f"Ошибка при конвертации файла: {e}")
-
-# Конвертируем перед запуском приложения
-ui_file = "login.ui"
-py_file = "login.py"
-convert_ui_to_py(ui_file, py_file)
-
-# Импортируем UI из сконвертированного файла
-from login import Ui_Dialog
 
 class LoginDialog(QDialog):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
+        self.ui = uic.loadUi('.ui/login.ui', self)
 
         # Привязка событий кнопок
         self.ui.registerButton.clicked.connect(self.register)
@@ -38,6 +21,7 @@ class LoginDialog(QDialog):
         username = self.ui.usernameLineEdit.text()
         password = self.ui.passwordLineEdit.text()
         print(f"Вход: {username}, {password}")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
