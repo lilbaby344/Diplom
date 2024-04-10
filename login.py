bfa69a0cc2af510e1ed3194b002a6cf4744376b1
Laura <kuttumuratovaa@mail.ru>
2024-04-10 12:07:22 +0300

    фон добавила

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QDialog
+from PyQt6.QtGui import QPixmap, QPalette, QBrush
 from PyQt6 import uic
 
 
@@ -8,14 +9,25 @@ class LoginDialog(QDialog):
         super().__init__()
         self.ui = uic.loadUi('.ui/login.ui', self)
 
+        self.setBackground('1.png')
+
         # Привязка событий кнопок
         self.ui.registerButton.setText("Зарегистрироваться")
         self.ui.loginButton.setText("Войти")
         self.ui.reg.setText("Нет аккаунта?")
         self.ui.vhod.setText("Авторизация")
 
+        self.vhod.setStyleSheet("font-size: 20pt;")
+
         self.ui.registerButton.clicked.connect(self.register)
         self.ui.loginButton.clicked.connect(self.login)
+
+    def setBackground(self, imagePath):
+        pixmap = QPixmap(imagePath)
+        brush = QBrush(pixmap)
+        palette = QPalette()
+        palette.setBrush(QPalette.ColorRole.Window, brush)
+        self.setPalette(palette)
 
     def register(self):
         username = self.ui.usernameLineEdit.text()
